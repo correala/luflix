186f2b7509b7ead0639a26371468471be11c2a9a
correala <correala@hotmail.com>
2020-07-28 20:45:31 -0300

    Aula 2 - imersão alura

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import './Menu.css'
 import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Luflix logo"></img>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
